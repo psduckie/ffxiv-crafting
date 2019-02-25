51c2a52c1fe4f8a3e41af2d11835487166839f1b
PSDuckie <psduckie@yahoo.com>
2019-02-25 13:52:49 -0700

    Added class for CSV entry.
    Also put in a simple CSV for future parsing.

---
+++ CraftFFXIV/src/duck/craftffxiv/CsvEntry.java
@@ -0,0 +1,19 @@
+package duck.craftffxiv;
+
+public class CsvEntry {
+	private String item;
+	private int needed;
+	
+	public String getItem() {
+		return item;
+	}
+	public void setItem(String item) {
+		this.item = item;
+	}
+	public int getNeeded() {
+		return needed;
+	}
+	public void setNeeded(int needed) {
+		this.needed = needed;
+	}
+}
